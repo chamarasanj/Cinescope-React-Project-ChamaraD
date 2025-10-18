7351695c1f6da005246da938966387718eba286f
Chamara Dissanayaka <chamarasanj@gmail.com>
2025-10-18 17:34:53 +0530

    test commit 4

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
-
 //RSC - React Server Component
 // SSR - Server Side Rendering  Enabled by default in Next.js 13 app directory
 export default function HomePage() {
   return (
-    <div>
+    <div className="text-center>">
+      <header className="bg-blue-500 text-white h-20">
+        This is my header
+      </header>
 
+      <main className="bg-purple-700 h-800>">test page - chamara</main>
+
+      <footer className="bg-gray-800 h-50">
+        This is my footer
+      </footer>
     </div>
   );
 }
