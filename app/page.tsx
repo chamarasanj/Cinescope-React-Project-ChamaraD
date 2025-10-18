3fe800b8e341303559ad8d36e0ddc92f98b1ab0e
Chamara Dissanayaka <chamarasanj@gmail.com>
2025-10-18 12:17:14 +0530

    small test modification

--- app/page.tsx
+++ app/page.tsx
@@ -13,6 +13,8 @@ export default function Home() {
           priority
         />
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
+          <li className="mb-2 tracking-[-.01em]">Test Project - Chamara</li>
+
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
