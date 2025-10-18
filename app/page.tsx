90187d8614e8b72f40833a9ff924ffac26ea45d1
Chamara Dissanayaka <chamarasanj@gmail.com>
2025-10-18 17:48:37 +0530

    adding flex style to div

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,14 @@
 // SSR - Server Side Rendering  Enabled by default in Next.js 13 app directory
 export default function HomePage() {
   return (
-    <div className="text-center>">
-      <header className="bg-blue-500 text-white h-20">
+    <div className="flex flex-col text-center min-h-screen">
+      <header className="flex-1 bg-blue-500 text-white h-20">
         This is my header
       </header>
 
-      <main className="bg-purple-700 h-800>">test page - chamara</main>
+      <main className="flex-12 bg-purple-700 h-800>">test page - chamara</main>
 
-      <footer className="bg-gray-800 h-50">
+      <footer className="flex-4 bg-gray-800 h-50">
         This is my footer
       </footer>
     </div>
