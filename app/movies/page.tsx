b74124ef16bc486554048dfa53883bb672f43cff
Chamara Dissanayaka <chamarasanj@gmail.com>
2025-10-18 19:03:02 +0530

    Add Link Button for routing

--- app/movies/page.tsx
+++ app/movies/page.tsx
@@ -1,11 +1,20 @@
 //RSC - React Server Component
 // SSR - Server Side Rendering  Enabled by default in Next.js 13 app directory
+
+import Link from "next/link";
 export default function MoviesPage() {
   return (
     <div className="flex flex-col text-center min-h-screen">
     
 
-      <main className="flex-12 bg-purple-700 h-800>">This is Movies page</main>
+      <main className="flex-12 bg-purple-700 h-800 text-4xl py-16>">This is Movies page
+
+
+        <Link   href="/" className="text-white text-sm bg-black p-4 rounded-full">
+          Go to Home Page
+        </Link>
+
+      </main>
 
       <footer className="flex-4 bg-gray-800 h-50">
         This is my footer
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,18 @@
 //RSC - React Server Component
 // SSR - Server Side Rendering  Enabled by default in Next.js 13 app directory
+
+import Link from "next/link";
 export default function HomePage() {
   return (
     <div className="flex flex-col text-center min-h-screen">
-     
-      <main className="flex-12 bg-purple-700 h-800>">This is Home Page</main>
+      <main className="flex-12 flex-col justify-center bg-purple-700 items-center gap-4>">
+        This is Home Page
+        <Link   href="/movies" className="text-white text-sm bg-black p-4 rounded-full">
+          Go to Movies Page
+        </Link>
+      </main>
 
-      <footer className="flex-4 bg-gray-800 h-50">
-        This is my footer
-      </footer>
+      <footer className="flex-4 bg-gray-800 h-50">This is my footer</footer>
     </div>
   );
 }
