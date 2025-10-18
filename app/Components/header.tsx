3e9fa2b93825ed767e016c59b3b937bd8ff16a10
Chamara Dissanayaka <chamarasanj@gmail.com>
2025-10-18 18:21:42 +0530

    add header component

---
+++ app/Components/header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+
+    return(
+      <header className="flex-1 bg-blue-500 text-white h-20">
+     Header page
+      </header>
+      )
+
+}
--- app/movies/page.tsx
+++ app/movies/page.tsx
@@ -1,10 +1,11 @@
 //RSC - React Server Component
 // SSR - Server Side Rendering  Enabled by default in Next.js 13 app directory
-export default function HomePage() {
+export default function MoviesPage() {
   return (
     <div className="flex flex-col text-center min-h-screen">
-     
-      <main className="flex-12 bg-purple-700 h-800>">This is Home Page</main>
+    
+
+      <main className="flex-12 bg-purple-700 h-800>">This is Movies page</main>
 
       <footer className="flex-4 bg-gray-800 h-50">
         This is my footer
