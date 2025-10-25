d0352a69c89c0fb14c37fc620f97810a8e9430d0
Chamara Dissanayaka <chamarasanj@gmail.com>
2025-10-25 09:54:23 +0530

    Demonstrate flex example

---
+++ app/genres/page.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function  GenresPage() {
+  return (
+    <div className='flex flex-col md:flex-row bg-purple-300 min-h-screen   '>
+        {/* <div className='h-20 w-30 p-6 bg-green-200 rounded text-center '>Div 1</div>
+        <div className='h-20 w-40 bg-blue-500 p-6 rounded text-center'>Div 2</div>
+        <div className='h-20 w-40 bg-red-500 p-6 rounded text-center'>Div 3</div>
+        <div className='h-20 w-40 bg- bg-purple-500 p-6 rounded text-center'>Div 4</div> */}
+
+
+        <div className='flex-1  bg-green-400 p-6 rounded text-center'> div 1 </div>
+        <div className='flex-1  bg-blue-400 p-6 rounded text-center'> div 2 </div>
+        <div className='flex-1  bg-red-400 p-6 rounded text-center'> div 3</div>
+        <div className='flex-1  bg-orange-400 p-6 rounded text-center'> div 4 </div>
+
+
+    </div>
+  )
+}
